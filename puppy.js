f5e0717fe848cb69429878cab0d2acc089b97d55
Patrick <patrickdavidml@gmail.com>
2020-04-02 20:15:46 +0100

    fix hashtags

--- puppy.js
+++ puppy.js
@@ -20,7 +20,7 @@
         var all = document.getElementsByTagName("span");
         for (var i=0, max=all.length; i < max; i++) {
            var num = mySelection[Math.floor(Math.random()*mySelection.length)]
-            if(all[i].innerHTML){all[i].innerHTML =  all[i].innerHTML.replace(/coronavirus|covid-19|corona|covid|virus|quarantine/gi,  `<img src="${num}" alt="puppy gif" height="120" width="200">`)}}
+            if(all[i].innerHTML){all[i].innerHTML =  all[i].innerHTML.replace(/\bcoronavirus\b|\bcovid-19\b|\bcorona\b|\bcovid\b|\bvirus\b|\bquarantine\b/gi,  `<img src="${num}" alt="puppy gif" height="120" width="200">`)}}
       
       }, 1000);
     
